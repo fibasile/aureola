06f2f8b437a8b4d7bd59fa0b3b436fc307e8c647
Fiore Basile <fiore.basile@gmail.com>
2019-02-10 09:45:54 +0100

    initial import

---
+++ assets/built/hamburger.js
@@ -0,0 +1,2 @@
+$(document).ready(function(){$(".navbar-burger").click(function(){$(".navbar-burger").toggleClass("is-active"),$(".navbar-menu").toggleClass("is-active")})});
+//# sourceMappingURL=hamburger.js.map
